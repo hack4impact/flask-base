8c5e9481660642688a06ec2b784fd8ed962610f6
Ben Sandler <ben1sandler@gmail.com>
2015-10-24 13:24:42 -0400

    Index should not be unique; we can have two classes of users which go to the same index

--- app/models/user.py
+++ app/models/user.py
@@ -15,7 +15,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    index = db.Column(db.String(64), unique=True)
+    index = db.Column(db.String(64))
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
