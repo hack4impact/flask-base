b6ab366e42ec4f3c436175b2bec0e333f36bd1dc
Abhi <suriabhinav1997@gmail.com>
2016-07-07 16:50:56 -0400

    utils commenting

--- app/utils.py
+++ app/utils.py
@@ -1,19 +1,32 @@
 from flask import url_for
 
+from wtforms.fields import HiddenField
+
 
 def register_template_utils(app):
     """Register Jinja 2 helpers (called from __init__.py)."""
 
+    # the template_test decorator allows us to add tests for
+    # conditional statements into jinja. For the test below,
+    # {% if x is equalto y %} blah {% endif %}
     @app.template_test()
     def equalto(value, other):
         return value == other
 
+    # This adds a global method. Can be accessed with
+    # {{ is_hidden_field(field) }}
     @app.template_global()
     def is_hidden_field(field):
-        from wtforms.fields import HiddenField
         return isinstance(field, HiddenField)
 
+    # see below, but this will add another template global
+
     app.add_template_global(index_for_role)
+
+# Provides the link to the index page for a created role...
+# If it works...
+# which I cannot verify at the moment
+# but it's there. So yay
 
 
 def index_for_role(role):
