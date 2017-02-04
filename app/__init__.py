b8be37d4aab8df6b0da13b1fe761b2cc025af66a
Abhi <suriabhinav1997@gmail.com>
2017-02-03 21:51:09 -0500

    updated inidividual packages

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
-from flask.ext.mail import Mail
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from flask.ext.assets import Environment
-from flask.ext.wtf import CsrfProtect
-from flask.ext.compress import Compress
-from flask.ext.rq import RQ
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_assets import Environment
+from flask_wtf import CsrfProtect
+from flask_compress import Compress
+from flask_rq import RQ
 
 from config import config
 from .assets import app_css, app_js, vendor_css, vendor_js
@@ -17,17 +17,19 @@ mail = Mail()
 db = SQLAlchemy()
 csrf = CsrfProtect()
 compress = Compress()
+
 # Set up Flask-Login
 login_manager = LoginManager()
-# TODO: Ideally this should be strong, but that led to bugs. Once this is
-# fixed, switch protection mode back to 'strong'
-login_manager.session_protection = 'basic'
+login_manager.session_protection = 'strong'
 login_manager.login_view = 'account.login'
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # not using sqlalchemy event system, hence disabling it
+
     config[config_name].init_app(app)
 
     # Set up extensions
