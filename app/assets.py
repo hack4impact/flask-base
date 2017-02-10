5904e41a932c8c859812370bd535f118a28404a9
Abhi <suriabhinav1997@gmail.com>
2017-02-10 17:15:35 -0500

    reordering script bundling

--- app/assets.py
+++ app/assets.py
@@ -1,9 +1,15 @@
 from flask_assets import Bundle
 
-app_css = Bundle('*.scss', filters='scss', output='styles/app.css')
+app_css = Bundle('app.scss', filters='scss', output='styles/app.css')
 
 app_js = Bundle('app.js', filters='jsmin', output='scripts/app.js')
 
-vendor_css = Bundle('vendor/*.css', output='styles/vendor.css')
+vendor_css = Bundle('vendor/semantic.min.css', output='styles/vendor.css')
 
-vendor_js = Bundle('vendor/*.js', filters='jsmin', output='scripts/vendor.js')
+vendor_js = Bundle(
+    'vendor/jquery.min.js',
+    'vendor/semantic.min.js',
+    'vendor/tablesort.min.js',
+    'vendor/zxcvbn.js',
+    filters='jsmin',
+    output='scripts/vendor.js')
