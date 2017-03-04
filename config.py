8d5cd225bd9c01ef317605f73e46d0dad1146d07
Abhi <suriabhinav1997@gmail.com>
2017-03-03 20:03:45 -0600

    fixed issue #123

--- config.py
+++ config.py
@@ -10,6 +10,13 @@ else:
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+if os.path.exists('config.env'):
+    print('Importing environment from .env file')
+    for line in open('config.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 
 class Config:
     APP_NAME = 'Flask-Base'
--- manage.py
+++ manage.py
@@ -11,12 +11,6 @@ from rq import Connection, Queue, Worker
 from app import create_app, db
 from app.models import Role, User
 
-if os.path.exists('config.env'):
-    print('Importing environment from .env file')
-    for line in open('config.env'):
-        var = line.strip().split('=')
-        if len(var) == 2:
-            os.environ[var[0]] = var[1]
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
