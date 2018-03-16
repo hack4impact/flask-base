dea9026415dffc2c4b0c27f371ba8e38a6715755
Santiago Buenahora <santib97@gmail.com>
2018-03-16 16:11:39 -0400

    fix styling issues + allow restrict custom

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,7 @@ from wtforms.fields import Field
 from wtforms.widgets import HiddenInput
 from wtforms.compat import text_type
 
+
 def register_template_utils(app):
     """Register Jinja 2 helpers (called from __init__.py)."""
 
@@ -25,10 +26,12 @@ def index_for_role(role):
 class CustomSelectField(Field):
     widget = HiddenInput()
 
-    def __init__(self, label='', validators=None, multiple=False, choices=[], **kwargs):
+    def __init__(self, label='', validators=None, multiple=False, 
+                 choices=[], allow_custom=True, **kwargs):
         super(CustomSelectField, self).__init__(label, validators, **kwargs)
         self.multiple = multiple
         self.choices = choices
+        self.allow_custom = allow_custom
 
     def _value(self):
         return text_type(self.data) if self.data is not None else ''
