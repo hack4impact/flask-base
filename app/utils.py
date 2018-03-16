c9b6e956f0e0b2c86a24f52b6960cbc8d54b7c0b
Santiago Buenahora <santib97@gmail.com>
2018-03-16 15:57:17 -0400

    Add customselect

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
 from flask import url_for
-
+from wtforms.fields import Field
+from wtforms.widgets import HiddenInput
+from wtforms.compat import text_type
 
 def register_template_utils(app):
     """Register Jinja 2 helpers (called from __init__.py)."""
@@ -18,3 +20,22 @@ def register_template_utils(app):
 
 def index_for_role(role):
     return url_for(role.index)
+
+
+class CustomSelectField(Field):
+    widget = HiddenInput()
+
+    def __init__(self, label='', validators=None, multiple=False, choices=[], **kwargs):
+        super(CustomSelectField, self).__init__(label, validators, **kwargs)
+        self.multiple = multiple
+        self.choices = choices
+
+    def _value(self):
+        return text_type(self.data) if self.data is not None else ''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = valuelist[1]
+            self.raw_data = [valuelist[1]]
+        else:
+            self.data = ''
