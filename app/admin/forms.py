8c0de1a63d7ffa4c8d89900935083ce39b3693bb
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-04-23 11:40:14 +0300

    :shirt: Refactor admin Blueprint

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,12 +1,21 @@
 from flask_wtf import Form
 from wtforms import ValidationError
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.fields import PasswordField, StringField, SubmitField
+from wtforms.fields import (
+    PasswordField,
+    StringField,
+    SubmitField,
+)
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import Email, EqualTo, InputRequired, Length
+from wtforms.validators import (
+    Email,
+    EqualTo,
+    InputRequired,
+    Length,
+)
 
-from .. import db
-from ..models import Role, User
+from app import db
+from app.models import Role, User
 
 
 class ChangeUserEmailForm(Form):
