c786004ceeb41975025d2e6f58227d4a12e9c63e
Nick Kocharhook <nick@kocharhook.com>
2019-09-02 16:35:15 +0100

    Update everything to the latest version. Add pipupdate to make this easy in future.
    Adopt FlaskForm and CSRFProtect to avoid deprecation warnings. Tweak config in create_app() so code works with Flask 1.0+.

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,16 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_rq import RQ
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CsrfProtect
+from flask_wtf import CSRFProtect
 
 from app.assets import app_css, app_js, vendor_css, vendor_js
-from config import config
+from config import config as Config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 mail = Mail()
 db = SQLAlchemy()
-csrf = CsrfProtect()
+csrf = CSRFProtect()
 compress = Compress()
 
 # Set up Flask-Login
@@ -25,13 +25,18 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'account.login'
 
 
-def create_app(config_name):
+def create_app(config):
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
+    config_name = config
+
+    if not isinstance(config, str):
+        config_name = os.getenv('FLASK_CONFIG', 'default')
+
+    app.config.from_object(Config[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # not using sqlalchemy event system, hence disabling it
 
-    config[config_name].init_app(app)
+    Config[config_name].init_app(app)
 
     # Set up extensions
     mail.init_app(app)
